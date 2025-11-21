6cf0e33f6b846242cd91bb49cd7b5b97b9ed1d2a
Dennis Liu <dennisliu010906@gmail.com>
2025-11-21 00:21:41 -0500

    added work with us button and animations

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
+import { Button } from "./ui/button";
 
 export default function HeroSection() {
+
+  const CULTUREANIMATION = "motion-delay-1000 motion-translate-x-in-[0%] motion-translate-y-in-[160%] motion-opacity-in-[0%] motion-duration-[1.33s] motion-duration-[1.87s]/opacity motion-delay-[1.05s]/opacity"
+  const ARTANIMATION = "motion-delay-1000 motion-translate-x-in-[0%] motion-translate-y-in-[-160%] motion-opacity-in-[0%] motion-duration-[1.33s] motion-duration-[1.87s]/opacity motion-delay-[1.05s]/opacity"
+  const DASHANIMATION = "motion-scale-x-in-0 motion-duration-[2.50s] motion-delay-500"
+  const AVANTANIMATION = "motion-translate-x-in-0 motion-delay-800 motion-translate-y-in-[-160%] motion-opacity-in-[0%] motion-duration-1500 motion-duration-[2.28s]/opacity motion-delay-[1.2s]/opacity"
+
   return (
     <div className="relative h-screen w-full font-montserrat ">
       {/* Background Image */}
@@ -16,18 +23,18 @@ export default function HeroSection() {
       {/* Hero Content */}
       <div className="absolute inset-0 z-10 w-full flex items-center justify-center">
         <div className="font-light w-full justify-between text-primary text-8xl xl:text-9xl flex flex-col xl:flex-row items-center gap-8 lg:gap-16 px-32 ">
-          <div className="tracking-widest motion-translate-x-in-0 motion-delay-500 motion-translate-y-in-[-160%] motion-opacity-in-[0%] motion-duration-1500 motion-duration-[1.98s]/opacity motion-delay-[0.72s]/opacity">AVANT</div>
-          <div className="motion-scale-x-in-0 motion-duration-[2.50s] motion-delay-500">—</div>
+          <div className={`tracking-widest ${AVANTANIMATION}`}>AVANT</div>
+          <div className={DASHANIMATION}>—</div>
           <div className="flex flex-col items-center gap-8">
             <div className="font-blosta font-light text-primary text-center mb-32">
-              <div className="whitespace-nowrap -mb-20 motion-delay-700 motion-translate-x-in-[0%] motion-translate-y-in-[-160%] motion-opacity-in-[0%] motion-duration-[1.13s] motion-duration-[1.67s]/opacity motion-delay-[0.85s]/opacity flex flex-row">Art x</div>
+              <div className={`whitespace-nowrap -mb-20 flex flex-row ${ARTANIMATION}`}>Art x</div>
               <br />
-              <div className="motion-delay-700 motion-translate-x-in-[0%] motion-translate-y-in-[160%] motion-opacity-in-[0%] motion-duration-[1.13s] motion-duration-[1.67s]/opacity motion-delay-[0.85s]/opacity flex flex-row">Culture</div>
+              <div className={`flex flex-row ${CULTUREANIMATION}`}>Culture</div>
             </div>
 
-            <button className="absolute bottom-16 mt-1 bg-black text-white px-6 py-3 text-xl rounded">
-              button
-            </button>
+            <Button className="tracking-[0.3em] font-fira-code text-xl px-8 py-8 motion-delay-[1.8s] motion-blur-in-md motion-opacity-in-0">
+              WORK WITH US
+            </Button>
           </div>
         </div>
       </div>
