aa4311153e0cc5c22c4db33ecf83f80b42a3007a
Ryan Yang <yang.rya@northeastern.edu>
2025-11-24 15:08:36 -0500

    Start about me

---
+++ components/about.tsx
@@ -0,0 +1,11 @@
+export default function HeroSection() {
+  return (
+    <section>
+        <h1>About Us</h1>
+        <p>Avant is an art consulting company specializing in private high end art sales/luxury goods and curated event experiences. 
+            Our team brings deep expertise in event strategy, production, influencer, engagment, and media coverage. WE partner with luxury brands, 
+            venues, and cultural institutions to execute high-impact activations that blend artistic vision with organizational excellence.
+        </p>
+    </section>
+  );
+}
